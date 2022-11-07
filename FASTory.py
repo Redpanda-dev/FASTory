40f22d2eafaa0b4fe4bd5208816bd31a749ba2b9
Fazsu <89864966+Fazsu@users.noreply.github.com>
2022-11-07 17:46:20 +0200

    Update FASTory.py
    updated workstations and initialized functions

--- FASTory.py
+++ FASTory.py
@@ -2,32 +2,56 @@ import requests
 import socket
 
 #{IP}/_services/{command}
-_services = "/rest/services"
+_services = "/rest/services/"
 _events = "/rest/events/"
+_ip = socket.gethostbyname(socket.gethostname())
 
-class workstation:
-    def move12():
+class Workstation:
+    def __init__(self,ip):
+        self.serviceurl = f'{ip}{_services}'
+        self.eventsurl = f'{ip}{_events}'
 
-        return 0
-
-    def move23():
-
-        return 0
-
-    def move35():
+    def move12(self):
 
         return 0
 
-    def move14():
+    def move23(self):
 
         return 0
 
-    def move45():
+    def move35(self):
+
+        return 0
+
+    def move14(self):
+
+        return 0
+
+    def move45(self):
 
         return 0
     
-    def get_status():
-        
+    def get_status(self):
+
+        return 0
+
+    def draw(self, receipe):
+
+        return 0
+    
+    #r="RED", g="GREEN", b="BLUE"
+    def select_pen(self, c):
+        if c == "r" or c == "RED":
+            s = "ChangePenRED"
+        elif c == "g" or c == "GREEN":
+            s = "ChangePenGREEN"
+        elif c == "b" or c == "BLUE":
+            s = "ChangePenBLUE"
+        else:
+            print("Undefined color")
+
+        command = f'{self.serviceurl}{s}'
+        requests.post(url=command)
         return 0
 
 def check_IP():
@@ -41,7 +65,7 @@ def check_IP():
 def main():
     ip = check_IP()
     print("Currently connected to ", ip)
-
+    Workstation(ip).select_pen("r")
     return 0
     
 main()
